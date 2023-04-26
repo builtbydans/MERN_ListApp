66d7d9e06a75ccc6349236aa23ea0f960e92d155
Danish Shafi <danish.shafi@live.co.uk>
2023-04-26 18:36:31 +0100

    Database config complete, client React skeleton created

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const env = require('dotenv').config();
+const cors = require('cors');
 const UserModel = require("./models/Users");
+
+app.use(express.json())
+app.use(cors());
 
 mongoose.connect(
   `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PW}@cluster0.i2bboww.mongodb.net/MERNListApp?retryWrites=true&w=majority`
@@ -14,7 +18,20 @@ app.get("/getUsers", async (req, res) => {
     res.send(users);
     console.log(users);
   } catch(err) {
-    console.log(err)
+    console.log(err);
+  }
+});
+
+app.post("/createUser", async (req, res) => {
+  try {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+
+  } catch(err) {
+    console.log("Failed to create a new user - ", err);
   }
 });
 
