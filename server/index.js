4efa23e626dbfcfa5e6c9390b6c091dbf824ccce
Danish Shafi <danish.shafi@live.co.uk>
2023-04-26 04:45:29 +0100

    MongoDB database succesfully connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const env = require('dotenv').config();
+const UserModel = require("./models/Users");
 
-mongoose.connect(`mongodb+srv://dan12345:${process.env.MONGODB_PW}@cluster0.i2bboww.mongodb.net/test`)
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PW}@cluster0.i2bboww.mongodb.net/MERNListApp?retryWrites=true&w=majority`
+);
+
+app.get("/getUsers", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.send(users);
+    console.log(users);
+  } catch(err) {
+    console.log(err)
+  }
+});
 
 app.listen(3001, () => {
   console.log('Server running succesfully on localhost:3001')
-})
+});
